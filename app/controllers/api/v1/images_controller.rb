8a70e4558361fcb98469edd4cad6a675c3ebf6a3
Ward Price <wardprice@gmail.com>
2020-10-09 15:35:24 -0400

    Update images_controller.rb

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -16,5 +16,3 @@ class Api::V1::ImagesController < ApplicationController
     params.require(:image).permit(:title)
   end
 end
-
-secret_key_base: 342a276ca171aae28ef198de6c713cc1e4329f62de7b280f49e12a2d241d43fdccae858c4cd04894c9f8b23a9e93fecf209837b60f3509611b8be158c3ad12fd
