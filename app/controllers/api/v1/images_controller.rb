f616a96e1a9452e2eea479b28c09dad9118036e3
woink <wardprice@gmail.com>
2020-10-09 23:37:59 -0400

    upload working

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::ImagesController < ApplicationController
   
+  def index
+    images = Image.all
+  end
+  
   def create
     # byebug
     image = Image.new(image_params)
     if params[:img_src] != ''
       image.img_src.attach(params[:img_src])
+      image.save
       image.img_url = url_for(image.img_src)
       image.save
     end
@@ -17,4 +22,3 @@ class Api::V1::ImagesController < ApplicationController
   end
 end
 
-secret_key_base: 342a276ca171aae28ef198de6c713cc1e4329f62de7b280f49e12a2d241d43fdccae858c4cd04894c9f8b23a9e93fecf209837b60f3509611b8be158c3ad12fd
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
   has_many :board_images
   has_many :boards, through: :board_images
-  has_one_attached :img_src 
+  has_one_attached :img_src
 end
